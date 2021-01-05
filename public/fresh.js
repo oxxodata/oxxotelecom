8b38e93f2aff97e264feef1e74fe6fe431702cfc
OGFrsi <ilyesou.cherfaoui.18@gmail.com>
2021-01-05 06:36:57 +0100

    Added chat

--- public/fresh.js
+++ public/fresh.js
@@ -184,3 +184,13 @@ $(document).ready(function () {
             }
         });
 })
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ff346c0c31c9117cb6b5d9c/1er74oo5h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
