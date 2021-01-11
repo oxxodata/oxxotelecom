f03fa352414d09706dac1591ee9c6874dec4c8c9
Lyes Cherfaoui <lyes@datagix.local>
2021-01-11 10:30:07 +0100

    update

--- pages/trunksip.js
+++ pages/trunksip.js
@@ -46,35 +46,6 @@ export default function trunksip() {
                 </div>
             </section>
 
-
-            <section className="section is-medium hero">
-                <div className="snip1214" style={{ margin: 'auto' }}>
-                    <div className="plan">
-                        <h3 className="plan-title">
-                            Pack M
-                        </h3>
-                        <div className="plan-cost"><span className="plan-price">10€</span><span className="plan-type">/ Mois</span></div>
-                        <ul className="plan-features">
-                            <li><i className="ion-checkmark"> </i>Engagement 12 mois</li>
-                            <li><i className="ion-checkmark"> </i>Nombre de cannaux 1 à 10</li>
-                        </ul>
-                        <div className="plan-select"><a href="https://market.oxxodata.com/cart.php?a=add&pid=173">Commander</a></div>
-                    </div>
-                    <div className="plan featured">
-                        <h3 className="plan-title">
-                            Pack L
-                        </h3>
-                        <div className="plan-cost"><span className="plan-price">24€</span><span className="plan-type">/ Mois</span></div>
-                        <ul className="plan-features">
-                            <li><i className="ion-checkmark"> </i>Sans engagement</li>
-                            <li><i className="ion-checkmark"> </i>Nombre de lignes illimités</li>
-                        </ul>
-                        <div className="plan-select"><a href="https://market.oxxodata.com/cart.php?a=add&pid=179">Commander</a></div>
-                    </div>
-                </div>
-            </section>
-
-
             <section className="section section-feature-grey is-medium">
                 <div className="container">
                     <div className="title-wrapper has-text-centered" style={{ maxWidth: '1000px' }}>
